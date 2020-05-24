2092649228df5b44165e5490177ca1bbcc0d828d
Nick <nsardonini@gmail.com>
2020-05-24 13:37:06 -0400

    create constructor & #create_board
    constructor creates board variable, which calls #create_board to provide it's value.
    the #create_board supplies nested arrays representing the coordinates of the board.

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,16 @@
 class Board
+  def initialize
+    @board = create_board
+  end
 
+  private
+  def create_board
+    board = []
+    8.times do |row|
+      8.times do |column|
+        board << [row, column]
+      end
+    end
+    board
+  end
 end
