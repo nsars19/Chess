6242831c7a94ee39932a9daf6a419d36fe787629
Nick <nsardonini@gmail.com>
2020-05-25 17:53:57 -0400

    add #get_(piece) methods. add #set_pawns

--- lib/board.rb
+++ lib/board.rb
@@ -11,6 +11,26 @@ class Board
   end
 
   private
+  def set_board
+    set_white_pieces
+    set_black_pieces
+  end
+
+  def set_pawns color
+    pawns = get_pawns color
+    pawn_row = {white: 2, black: 7}
+    chosen = pawn_row[color]
+    ('a'..'h').each { |letter| @board.tiles["#{letter}#{chosen}"] = pawns.pop }
+  end
+
+  [Pawn, Rook, Bishop, Knight].each do |piece|
+    define_method("get_#{piece.to_s.downcase}s") do |white_black|
+      piece_set = {white: 0, black: 1}
+      chosen = piece_set[white_black]
+      pieces = @pieces[chosen].select { |item| item.is_a? piece }
+    end
+  end
+
   def create_pieces
     pieces = [King.new, Queen.new]
     2.times { pieces << Bishop.new << Knight.new << Rook.new }
