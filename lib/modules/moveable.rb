3c1ae4d8fcc84d3730fe01a06a07a1a69b35b4cd
Nick <nsardonini@gmail.com>
2020-05-26 14:56:05 -0400

    add #move_piece & #find_piece
    rough outline for #move_piece to provide context for other methods to be created

--- lib/modules/moveable.rb
+++ lib/modules/moveable.rb
@@ -1,2 +1,13 @@
 module Moveable
+  def move_piece start, finish
+    piece = find_piece(start)
+    moves = get_moves(piece)
+    if bad_move?(start, finish) || !moves.include?(finish)
+      reselect()
+    end
+  end
+
+  def find_piece(node, board)
+    board[node]
+  end
 end
