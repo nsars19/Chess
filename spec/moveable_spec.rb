7c993369cc79d17b1faee953b79f8a964bd02ec6
Nick <nsardonini@gmail.com>
2020-05-27 16:32:38 -0400

    change example setup

--- spec/moveable_spec.rb
+++ spec/moveable_spec.rb
@@ -2,16 +2,13 @@ require './lib/main'
 
 describe "Moveable" do
   describe "#move_piece" do
+    let(:game) { Game.new }
+
     it "changes hash values of Board objects" do
-      pawn = double("pawn")
-      tiles = {'a2' => pawn, 'a3' => nil}
+      pawn = game.board.tiles['a2']
       player = double("player", :pieces => [pawn])
-      game = double("game",
-                    :player => player,
-                    :tiles => tiles
-                    )
-      expect(game.move_piece('a2', 'a3', player, game.tiles)).to(
-        change { game.tiles['a3'] }.from(nil).to(pawn)
+      expect(game.move_piece('a2', 'a3', player, game.board.tiles)).to(
+        change { game.board.tiles['a3'] }.from(nil).to(pawn)
       )
     end
   end
