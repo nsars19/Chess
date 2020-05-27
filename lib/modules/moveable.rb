93bb5a849f9b60c2d13402bfdb0635c53bdadcf9
Nick <nsardonini@gmail.com>
2020-05-27 17:22:47 -0400

    add #on_board?. add call to #on_board? from #reselect

--- lib/modules/moveable.rb
+++ lib/modules/moveable.rb
@@ -22,7 +22,7 @@ module Moveable
     return true unless belongs_to?(piece, player)
     [start, finish].each do |coord|
       char = coord.split('')
-      return true unless ('a'..'h').include? char[0]
+      return true unless on_board? coord
       return true unless (1..8).include? char[1]
     end
     false
@@ -33,6 +33,19 @@ module Moveable
   end
 
   def reselect
+    choice = gets.split
+    reselect if choice.size != 2
+    choice.each do |coord|
+      reselect unless on_board?(coord)
+    end
+    choice
+  end
+
+  def on_board? coord
+    chars = coord.split('')
+    return false if chars.size != 2
+    return true if ('a'..'h').include? char[0] && (1..8).include? char[1]
+    false
   end
 
   def get_moves(piece)
