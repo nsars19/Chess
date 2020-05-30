d57c3f5ad5673b7fd19e6ed80be02a54d683a4da
Nick <nsardonini@gmail.com>
2020-05-30 14:57:16 -0400

    change method chain to variable
    changed 'game.board.tiles' to 'board', as that variable holds the same value

--- spec/moveable_spec.rb
+++ spec/moveable_spec.rb
@@ -6,7 +6,7 @@ describe "Moveable" do
 
   describe "#move_piece" do
     xit "changes hash values of Board objects" do
-      pawn = game.board.tiles['a2']
+      pawn = board['a2']
       player = double("player", :pieces => [pawn], :color => :white)
       expect(game.move_piece('a2', 'a3', player, game.board.tiles)).to(
         change { game.board.tiles['a3'] }.from(nil).to(pawn)
