65cc889a85309146de6b0fa96d238d0558318da3
Nick <nsardonini@gmail.com>
2020-06-01 17:13:22 -0400

    add #promotable?
    checks if a pawn is eligible for promotion

--- lib/modules/checkable.rb
+++ lib/modules/checkable.rb
@@ -1,3 +1,12 @@
 module Checkable
-
+  # Checks current state of board and evaluates various conditions.
+  # eg. is king in check? can pawn be promoted?
+  def promotable?(coord, player, board)
+    colors = {white: 8, black: 1}
+    num = coord[1].to_i
+    if board[coord].is_a?(Pawn) && num == colors[player.color]
+      return true
+    end
+    false
+  end
 end
