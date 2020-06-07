15cebeb977b3a783e15c8c487008f6254c199387
Nick <nsardonini@gmail.com>
2020-06-07 18:18:16 -0400

    add #from_json

--- lib/modules/save_functionality/serialization.rb
+++ lib/modules/save_functionality/serialization.rb
@@ -7,4 +7,11 @@ module Serializable
     file = "../saves/#{filename}.json"
     File.open(file, 'w') { |file| file.puts @board.to_json }
   end
+  
+  def from_json string
+    string = string.gsub! 'null', 'nil'
+    data = JSON.load(string).each do |var, val|
+      self.instance_variable_set var, val
+    end
+  end
 end
