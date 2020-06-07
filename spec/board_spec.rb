d1e633fd10c96f7418e97d369267c6be12622f3b
Nick <nsardonini@gmail.com>
2020-06-07 17:21:28 -0400

    change example to reflect removal of @board
    changed example to test for hash creation instead of array creation. This is because @board was
    replaced with a local variable inside the definition of #create_cells.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,8 +4,8 @@ describe 'Board' do
   context 'when initialized' do
     let(:board) { Board.new }
     
-    it 'creates 64 size array' do
-      expect(board.board.size).to eql 64
+    it 'creates 64 size hash' do
+      expect(board.tiles.size).to eql 64
     end
   end
 end
