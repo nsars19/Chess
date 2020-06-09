17944f786fb6fd501c1bbe91c185a49a44b4e2b9
Nick <nsardonini@gmail.com>
2020-06-09 16:55:49 -0400

    add #offer_draw
    allows for a draw-game ending to be offered to the other player if #fifty_moves_rule? or #same_move_three_times?
    evaluate as truthy.

--- lib/modules/endable.rb
+++ lib/modules/endable.rb
@@ -1,5 +1,5 @@
 module Endable
-  def checkmate player
+  def checkmate(player)
     player_num = {white: 1, black: 2}
     "Checkmate! Player #{player_num[player.color]} loses."
     exit
@@ -10,8 +10,27 @@ module Endable
     exit
   end
 
+  def offer_draw(player, board)
+    if fifty_moves_rule?(board) || same_move_three_times?(player, board)
+      other_player = {white: 2, black: 1} # ask other player for draw, white is p1, so ask p2.
+      puts "Player #{other_player[player.color]} do you accept the draw? (y/n)"
+      
+      begin
+        answer = STDIN.gets.chomp
+        unless answer == 'y' || answer == 'n'
+          raise Exception.new("Please select 'y' or 'n'") 
+        end
+      rescue Exception => e
+        puts e
+        retry
+      end
+      
+      answer == 'y' ? draw : return
+    end
+  end
+
   def draw
-    "Draw game."
+    puts "\nDraw game."
     exit
   end
 end
