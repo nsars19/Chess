5898ef8e6de13ddc27efa656f2834f0db6daad3c
Nick <nsardonini@gmail.com>
2020-06-09 14:07:35 -0400

    add game ending methods #checkmate, #stalemate, and #draw
    The methods output text to stdout related to the game ending, and then exit the program.

--- lib/modules/endable.rb
+++ lib/modules/endable.rb
@@ -1,10 +1,17 @@
 module Endable
-  def checkmate
+  def checkmate player
+    player_num = {white: 1, black: 2}
+    "Checkmate! Player #{player_num[player.color]} loses."
+    exit
   end
 
   def stalemate
+    "Stalemate! The game is a draw."
+    exit
   end
 
   def draw
+    "Draw game."
+    exit
   end
 end
