b7098fe07767117791e92da39d8b619715f96055
Nick <nsardonini@gmail.com>
2020-06-14 17:11:05 -0400

    add default parameter
    for access at a higher context than the begin/rescue expression.
    used for control flow when moving out of load menu.

--- lib/main.rb
+++ lib/main.rb
@@ -206,7 +206,7 @@ class Game
     end
   end
 
-  def fetch_save_file
+  def fetch_save_file filename = nil
     Dir.children('./saves/').each { |file| print "#{file[0...-5]} "; print "\n" }
     begin
       filename = prompt_and_get_input("\nPlease select a file, or type 'exit' to go back to the main menu: ")[0]
